1193681dfbd37b53ee94e3937f0556c5f7279236
irakli nozadze <iraklinozadze2509@gmail.com>
2023-06-05 21:55:53 +0400

    add full functionality and about view

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Irakli Nozadze on 05.06.23.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,50 +10,89 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var slider: UISlider!
     @IBOutlet var targetLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var roundLabel: UILabel!
     
     var currentValue: Int = 50
-    var targetValue = 0
+    var targetValue: Int = 0
+    var score: Int = 0
+    var round: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startNewRound()
+        startNewGame()
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+        
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0,
+                                  left: 14,
+                                  bottom: 0,
+                                  right: 14)
+        
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = UIImage(named: "SliderTrackRight")!
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
     }
     
     @IBAction func showAlert() {
-        var difference: Int
-        if currentValue > targetValue {
-            difference = currentValue - targetValue
-        } else if targetValue > currentValue {
-            difference = targetValue - currentValue
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
+        let title: String
+        
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
         } else {
-            difference = 0
+            title = "Not even close..."
         }
         
-        let message = "The value of the slider is: \(currentValue)" +
-        "\nThe target value is: \(targetValue)"
+        score += points
+        
+        let message = "You scored \(points) points"
         
         let alert = UIAlertController(
-            title: "Bulls Eye",
+            title: title,
             message: message,
             preferredStyle: .alert
         )
         
         let action = UIAlertAction(
             title: "OK",
-            style: .default
+            style: .default,
+            handler: {_ in
+                self.startNewRound()
+            }
         )
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-        
-        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lround(Double(slider.value))
     }
-    
+    @IBAction func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
     func startNewRound() {
+        round += 1
         targetValue = Int.random(in: 1...100)
         currentValue = 50
         slider.value = Float(currentValue)
@@ -62,6 +101,8 @@ class ViewController: UIViewController {
     
     func updateLables() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 }
 
