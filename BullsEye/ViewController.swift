6bbdbbea4f3f2b9eefeeb15a207ba4a739a504cc
irakli nozadze <iraklinozadze2509@gmail.com>
2023-06-05 18:09:59 +0400

    Feat: add UI elemeents

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
+    
+    var currentValue: Int = 50
+    var targetValue = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startNewRound()
     }
-
-
+    
+    @IBAction func showAlert() {
+        var difference: Int
+        if currentValue > targetValue {
+            difference = currentValue - targetValue
+        } else if targetValue > currentValue {
+            difference = targetValue - currentValue
+        } else {
+            difference = 0
+        }
+        
+        let message = "The value of the slider is: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
+        
+        let alert = UIAlertController(
+            title: "Bulls Eye",
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(
+            title: "OK",
+            style: .default
+        )
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lround(Double(slider.value))
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLables()
+    }
+    
+    func updateLables() {
+        targetLabel.text = String(targetValue)
+    }
 }
 
